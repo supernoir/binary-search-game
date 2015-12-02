d32c389799f9b2a894e6b4e14054d50d898da687
supernoir <kawohl.bentodesign@gmail.com>
2015-12-03 00:10:53 +0100

    Builds the Algorithm, refactors

--- binarysearchgame_01.rb
+++ binarysearchgame_01.rb
@@ -1,26 +1,24 @@
 ## BINARY SEARCH GAME
-## by Rubynettes v0.1
+# by Rubynettes v0.1
 
-## Init
+# Initialization
 hi = 0
 lo = 101
 mid = 0
 q = 0
 step = 0
 
-## CLI INIT: Welcome - Get User Name and Prompt User for Number to be guessed
 
-## Welcome
+# Welcome
 puts "- " * 40
 puts "BINARY SEARCH GAME"
 puts "by Rubynettes v0.1\n"
 
-
-## Get User Name
+# Get User Name
 print "Please enter your name: "
 user = gets.chomp.to_s
 
-## Prompt User for Number
+# Prompt User for Number
 puts " "
 puts "Why hello, #{user}!"
 puts "Please give me a number that I can guess."
@@ -28,15 +26,27 @@ puts "The number needs to be between 1 and 100."
 print "Enter number: "
 usernumber = gets.chomp.to_i
 
-## Debugging - Show number
+
+# Debugging - Show number
 puts "I cheated. It's #{usernumber}"
+
 
 ## GUESSWORK:
 
+
+# Explain 
 def explain_step(lo,hi)
-puts "Now picking a number between #{lo} and #{hi}."
+	puts "Now picking a number between #{lo} and #{hi}."
 end
 
 explain_step(0,101)
 
+def calculating_mid(lo,hi)
+	mid = lo + (hi-lo) / 2
+	puts "Is it #{mid}?"
+	return mid
+end
+
+calculating_mid(lo,hi)
+explain_step(0,mid)
 
